1eafedbbe2defbe2ce99782f47481663abf2d9a3
Akshay-1010 <officialforak@gmail.com>
2023-09-26 23:56:07 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import dash
 from dash import Dash, html, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
 import plotly.express as px
 from flask import Flask
-
 import pandas as pd
 
-# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+df = pd.read_csv('tips.csv')
 server = Flask(__name__)
 app = dash.Dash(name = __name__, server = server)
-app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+app.layout = dbc.Container([
+    html.H1(children='Tips Dataset', style={'textAlign':'center'}),
+    html.H3(children=str([i for i in df.columns ]), style={'textAlign':'center'}),
     # dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
