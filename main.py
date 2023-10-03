f084dd6959f3c522a1ee890ea5cc216b7bd751f7
Akshay-1010 <officialforak@gmail.com>
2023-10-03 09:50:20 -0500

    adding multi page

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import plotly.io as pio
 from flask import Flask
 import pandas as pd
-
+dash.register_page(__name__, path='/')
 df = pd.read_csv('tips.csv')
 
 df['bill_percent'] = (df['tip'] / df['total_bill']) * 100
